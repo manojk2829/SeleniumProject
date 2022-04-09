e9c0feb4c50fa13426e5d068ddfcfaae19de6a02
manoj <manojk2829@gmail.com>
2022-04-09 13:04:10 +0530

    MyCommit Done

---
+++ src/test/java/sample_Project/TestClassStudy.java
@@ -0,0 +1,9 @@
+package sample_Project;
+
+public class TestClassStudy {
+
+	public static void main(String[] args) {
+		System.out.println("Manoj Kushwaha");
+	}
+
+}
---
+++ src/test/java/sample_Project/TestDDFClass.java
@@ -0,0 +1,11 @@
+package sample_Project;
+
+import org.testng.annotations.Test;
+
+public class TestDDFClass {
+
+	@Test
+	public void testmethod(){
+		System.out.println("GOod job");
+	}
+}
